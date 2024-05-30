70d783df34cf148be60a36d1a7ef6eaf3dd270bd
Elisa Gatet <elisa.gatet@pierre-fabre.com>
2024-05-30 12:43:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 # Title of the app
 st.title("Student Information")
 title = st.text_input("Enter the Student name", "Tintin")
-age = st.slider("Select the student's age", 1, 100,25)
+age = st.slider("Select the student's age", 2, 110,26)
 if st.button("Display Information"):
     st.write("Student's name: ",title)
     st.write("Student's age: ",age)
